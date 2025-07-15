661e3334a43f1338a01d2af385a84aaf5d0aced6
lucasaamorim <lucasapolonio@ufrn.edu.br>
2025-07-15 18:05:01 -0300

    setup

---
+++ learnflow/src/main/java/com/TrabalhoEngSoftware/learnflow/LearnflowApplication.java
@@ -0,0 +1,13 @@
+package com.TrabalhoEngSoftware.learnflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnflowApplication.class, args);
+	}
+
+}
---
+++ learnflow/src/test/java/com/TrabalhoEngSoftware/learnflow/LearnflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrabalhoEngSoftware.learnflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
